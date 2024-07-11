2909776fccb223fb910d2d646db1dced145215b7
redwan9011 <redwan390309@gmail.com>
2024-07-11 11:57:56 +0600

    complete footer

--- src/HomeComponents/Service/Service.jsx
+++ src/HomeComponents/Service/Service.jsx
@@ -10,7 +10,12 @@ const Service = () => {
                     <h1 className='text-4xl font-bold mt-5'>We Created best dinning experience for you & your family</h1>
                     <h6 className='text-yellow-500 font-semibold mb-10 mt-3'>SIMPLY BUT DELICIOUS EXPERIENCE</h6>
 
-                    <p className='text-gray-500 '>Morbi leo risus, porta ac consectetur ac, vesti bulum at eros. Aenean lacinia bibendum nulla sed consec us eget urna mollis ornare vel euleo. Nullam id dolor id nibh ultricies vehicula ut id elit. Cum sociis natoque penatibus et magnis dis parturient monte siculus mus.Maecenas sed diam eget risus varius blandit sit amet non magna.
+                    <p className='text-gray-500 '>At SEA QUEEN, we believe that dining out should be more than just a meal; it should be an experience. That’s why we’ve crafted an ambiance that’s perfect for families and friends to enjoy delightful moments together. Our chefs have meticulously designed a menu that brings together the freshest ingredients and flavors, ensuring every dish is a celebration of taste and quality.
+                            <br />
+                        From the moment you walk through our doors, you’ll be welcomed with warm hospitality and a comfortable, inviting atmosphere. Whether you’re here for a casual lunch, a special family dinner, or a celebration, we promise a memorable dining experience that’s both simple and delicious.
+
+
+                        Your table is ready, and we can’t wait to serve you!
                     </p>
 
                 </div>
@@ -18,11 +23,11 @@ const Service = () => {
 
 
                 <div className='grid grid-cols-2 gap-3 '>
-                <img src={img4} alt="seriviceImg" className='w-full'/>
-                    <img src={img1} alt="seriviceImg" className='w-full'/>
-                    <img src={img2} alt="seriviceImg" className='w-full'/>
-                    <img src={img3} alt="seriviceImg" className='w-full'/>
-                    
+                    <img src={img4} alt="seriviceImg" className='w-full' />
+                    <img src={img1} alt="seriviceImg" className='w-full' />
+                    <img src={img2} alt="seriviceImg" className='w-full' />
+                    <img src={img3} alt="seriviceImg" className='w-full' />
+
 
                 </div>
             </div>
